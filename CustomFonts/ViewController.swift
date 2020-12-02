c9a0a946c764b9545b2d8aa3f7f3f09e9f4535f5
27293 <myltd6@fpt.com.vn>
2020-12-02 12:13:31 +0700

    create project

--- CustomFonts/ViewController.swift
+++ CustomFonts/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        label.text = "My Font!"
+        view.addSubview(label)
+        label.center = view.center
+        label.font = .robotoLightItalic(size: 22)
     }
 
 
+}
+
+extension UIFont {
+    static func robotoLightItalic(size: CGFloat) -> UIFont {
+        return UIFont(name: "Roboto-LightItalic", size: size) ?? .systemFont(ofSize: 32)
+    }
+    
+    static func robotoBoldItalic(size: CGFloat) -> UIFont? {
+        return UIFont(name: "Roboto-BoldItalic", size: size)
+    }
 }
 
